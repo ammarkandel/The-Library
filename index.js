3f297b5f6e47370a313d04d6a808533316f12553
ammarkandel <merom8703@gmail.com>
2021-03-24 09:20:16 +0200

    Fix linter warning

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-let myLibrary = []
+const myLibrary = [];
 
 function Book(title, author, pages) {
-  this.title = title
-  this.author = author
-  this.pages = pages
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
 function addBooksToLibrary() {
-  const title = document.getElementById('title')
-  const author = document.getElementById('author')
-  const pages = document.getElementById('pages')
-  let newBook = new Book(title.value, author.value, pages.value)
-  const readBtn = document.createElement('button')
-  const rmBtn = document.createElement('button')
-  const displayBooks = document.querySelector('.display-books')
-  const li = document.createElement('li')
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const newBook = new Book(title.value, author.value, pages.value);
+  const readBtn = document.createElement('button');
+  const rmBtn = document.createElement('button');
+  const displayBooks = document.querySelector('.display-books');
+  const li = document.createElement('li');
 
-  readBtn.textContent = 'not read yet'
-  Book.prototype.status = function() {
-    readBtn.classList.toggle('true_value')
+  readBtn.textContent = 'not read yet';
+  Book.prototype.status = () => {
+    readBtn.classList.toggle('true_value');
     if (readBtn.textContent == 'not read yet') {
-      readBtn.textContent = 'already read'
-      return true
+      readBtn.textContent = 'already read';
+      return true;
     } else {
-      readBtn.textContent = 'not read yet'
-      return false
+      readBtn.textContent = 'not read yet';
+      return false;
     }
   }
 
-  myLibrary.push(newBook)
-  readBtn.addEventListener('click', newBook.status)
+  myLibrary.push(newBook);
+  readBtn.addEventListener('click', newBook.status);
 
-  const lastItem = myLibrary.pop()
-  li.textContent = 'Title: ' + lastItem.title + ' ,Author: ' + lastItem.author + ' ,Pages: ' + lastItem.pages
-  rmBtn.textContent = 'REMOVE BOOK'
-  displayBooks.appendChild(li)
-  li.appendChild(rmBtn)
-  li.appendChild(readBtn)
+  const lastItem = myLibrary.pop();
+  li.textContent = 'Title: ' + lastItem.title + ' ,Author: ' + lastItem.author + ' ,Pages: ' + lastItem.pages;
+  rmBtn.textContent = 'REMOVE BOOK';
+  displayBooks.appendChild(li);
+  li.appendChild(rmBtn);
+  li.appendChild(readBtn);
 }
 
-const newBookBtn = document.getElementById('new_book')
-const theForm = document.querySelector('#book_form')
-const submitBtn = document.getElementById('submit')
+const newBookBtn = document.getElementById('new_book');
+const theForm = document.querySelector('#book_form');
+const submitBtn = document.getElementById('submit');
 
-newBookBtn.addEventListener('click', _=> {
-  theForm.setAttribute("style", "display: block")
+newBookBtn.addEventListener('click', () => {
+  theForm.setAttribute("style", "display: block");
 })
 
-submitBtn.addEventListener('click', (e)=> {
-  e.preventDefault()
-  addBooksToLibrary()
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBooksToLibrary();
 })
